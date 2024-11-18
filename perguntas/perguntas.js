05b3d013a7a64748066c56cfaf678c256ea2fa08
Kaique Oliveira <149913015+kaiiquerl@users.noreply.github.com>
2024-11-17 22:50:10 -0300

    Add files via upload

---
+++ perguntas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em Python?',
+    'Uma variável é usada para armazenar dados na memória.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país do mundo é a Rússia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop em Python?',
+    'Um loop é uma estrutura de controle que repete um bloco de código várias vezes.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em inglês?',
+    'Obrigado em inglês é THANK YOU (THENK IÚ).'
+)
